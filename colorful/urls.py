ed1dda5d724d69b93327545874578e44590684a5
GabrielTeodoroSan <gabrielteodorodomingues@gmail.com>
2022-07-27 15:34:57 -0300

    Creating simple system and a template folder.

--- colorful/urls.py
+++ colorful/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('color/', views.colorize, name='colorize'),
 ]
--- colorful/views.py
+++ colorful/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('Hello, World')
+    return render(request, 'colorful/home.html')
+
+
+def colorize(request):
+    color = request.POST['color-name']
+    return render(request, 'colorful/color.html', {'color': color})
 
 
